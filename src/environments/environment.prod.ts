d8b596ecbedb7446dbebe8bf5a9d3e5f60fce469
tiagodepalves <tiagodepalves@gmail.com>
2019-05-01 00:28:24 -0300

    production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiURL: 'http://192.168.1.10:8000',
-  // apiURL: 'https://mpeapi.herokuapp.com',
+  // apiURL: 'http://192.168.1.10:8000',
+  apiURL: 'https://mpeapi.herokuapp.com',
   production: true
 };
